222e8a69b09d2a622ecbfac0266518b1d1ef51da
Minecraftehsugram <111742226+Minecraftehsugram@users.noreply.github.com>
2022-08-22 12:04:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const myName = 'Lorenz';
+const div = document.getElementById('nameDiv')
+
+function changeText(div)  {
+    div.innerText = `Hello my name is ${myName}`;
+}
+
+function buttonClicked() {
+    const currentTime = new Date();
+    const newElement = document.createElement('div');
+    newElement.innerText = `Created at ${currentTime}`;
+    document.body.appendChild(newElement);
+}
+changeText(div);
